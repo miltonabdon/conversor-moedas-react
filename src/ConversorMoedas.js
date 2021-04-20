8c95f281037f0844ebc71498f7717add8a1d5625
Milton <miltonabdon69@gmail.com>
2021-04-19 21:21:33 -0300

    adicionado alertas e modal ao front

--- src/ConversorMoedas.js
+++ src/ConversorMoedas.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./ConversorMoedas.css";
-import { Jumbotron, Button, Form, Col, Spinner } from "react-bootstrap";
+import {
+  Jumbotron,
+  Button,
+  Form,
+  Col,
+  Spinner,
+  Alert,
+  Modal,
+} from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons";
 
@@ -8,6 +16,9 @@ function ConversorMoedas() {
   return (
     <div>
       <h1>Conversor de Moedas</h1>
+      <Alert variant="danger" show={false}>
+        Erro obtendo dados de conversão, tente novamente
+      </Alert>
       <Jumbotron>
         <Form>
           <Form.Row>
@@ -17,7 +28,7 @@ function ConversorMoedas() {
             <Col sm="3">
               <Form.Control as="select"></Form.Control>
             </Col>
-            <Col sm="1" className="text-center" style={{paddingTop:'5px'}}>
+            <Col sm="1" className="text-center" style={{ paddingTop: "5px" }}>
               <FontAwesomeIcon icon={faAngleDoubleRight} />
             </Col>
             <Col sm="3">
@@ -25,11 +36,21 @@ function ConversorMoedas() {
             </Col>
             <Col sm="2">
               <Button variant="success" type="submit">
-                <Spinner animation="border" size="sm"/>
-                Converter</Button>
+                <Spinner animation="border" size="sm" />
+                Converter
+              </Button>
             </Col>
           </Form.Row>
         </Form>
+        <Modal show={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Conversão</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Resultado da conversão aqui...</Modal.Body>
+          <Modal.Footer>
+            <Button variant="success">Nova Conversão</Button>
+          </Modal.Footer>
+        </Modal>
       </Jumbotron>
     </div>
   );
