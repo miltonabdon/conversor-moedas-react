db004981e0291e0e0a0b140a318e8d659d189365
Milton <miltonabdon69@gmail.com>
2021-04-24 21:46:07 -0300

    implementacao de testes e correção testid

--- src/ConversorMoedas.js
+++ src/ConversorMoedas.js
@@ -129,7 +129,11 @@ function ConversorMoedas() {
               </Form.Control>
             </Col>
             <Col sm="2">
-              <Button variant="success" type="submit">
+              <Button
+                variant="success"
+                type="submit"
+                data-testid="btn-converter"
+              >
                 <span className={exibirSpinner ? null : "hidden"}>
                   <Spinner animation="border" size="sm" />
                 </span>
@@ -140,7 +144,11 @@ function ConversorMoedas() {
             </Col>
           </Form.Row>
         </Form>
-        <Modal show={exibirModal} onHide={handleFecharModal}>
+        <Modal
+          show={exibirModal}
+          onHide={handleFecharModal}
+          data-testid="modal"
+        >
           <Modal.Header closeButton>
             <Modal.Title>Conversão</Modal.Title>
           </Modal.Header>
--- src/ConversorMoedas.test.js
+++ src/ConversorMoedas.test.js
@@ -1,10 +1,27 @@
-import { render, screen } from "@testing-library/react";
-import ConversorMoedas from "./ConversorMoedas";
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ConversorMoedas from './ConversorMoedas';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import axiosMock from 'axios';
 
-test("deve rendezirar o componente sem erros", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<ConversorMoedas />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Teste do componente de conversão de moedas', () => {
+
+  it('deve renderizar o componente sem erros', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ConversorMoedas />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('deve simular uma conversão de moedas', async () => {
+    const { findByTestId, getByTestId } = render(<ConversorMoedas />);
+    axiosMock.get.mockResolvedValueOnce({
+      data: {success: true, rates: { BRL: 4.564292, USD: 1.101049 }}
+    });
+    fireEvent.click(getByTestId('btn-converter'));
+    const modal = await findByTestId('modal');
+    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+    expect(modal).toHaveTextContent('1 BRL = 0.24 USD');
+  });
+
 });
